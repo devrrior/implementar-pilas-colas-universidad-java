dc5722c3e30b22de792743e8d45b10790113a5e4
Fernando Guerrero <devrrior@gmail.com>
2022-01-30 16:15:52 -0600

    set up menu

--- libreria/LibreriaEjemplo.java
+++ libreria/LibreriaEjemplo.java
@@ -1,8 +1,34 @@
 package libreria;
 
 public class LibreriaEjemplo {
-  public static void main(String[] args) {
 
-    System.out.println("Hello world!");
+  public static void pedirLibros(){
+    
+    // aqui haces tu magia bro
+    // aqui se implementa la pila con los libros
+  }
+
+  public static void main(String[] args) {
+    boolean band = true;
+    int opt = 1;
+
+    do {
+      System.out.println("--- Bienvenido a libreria Quijote de la Mancha ---");
+      System.out.println("1. Anadir persona a la fila");
+      System.out.println("2. Remover persona de la fila");
+      System.out.println("3. Salir");
+
+      switch (opt) {
+        case 1:
+            Cola colaLibreria = new Cola(20);
+            colaLibreria.InsertarCola();
+            pedirLibros();        
+          break;
+        case 2:
+          break;
+        case 3:
+          break;
+      }
+    } while (band);
   }
 }
