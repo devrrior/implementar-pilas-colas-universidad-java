108fb4bf726ec7d2fe2690ec7456ea6836a5bd44
edualt <eduardosebastianhdz503@gmail.com>
2022-01-30 17:31:59 -0600

    hola

--- libreria/Pila.java
+++ libreria/Pila.java
@@ -1,5 +1,48 @@
 package libreria;
 
-public class Pilas {
+public class Pila {
+    String arreglo[];
+    int tamanomax;
+    int cima;
 
+    public Pila(int n){
+        tamanomax=n;
+        arreglo= new String[tamanomax];
+        cima=0;
+    }
+
+    public boolean vacio(){
+        if(cima==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public void cobrar(String str){
+        if(cima<tamanomax){
+            arreglo[cima]=str;
+            cima++;
+        }
+    }
+
+    public String mostrarCima(){
+        if(cima>0){
+            return arreglo[cima-1];
+        }
+        else{
+            return null;
+        }
+    }
+
+    public String cobrado(){
+        String temp=null;
+        if(cima>0){
+            temp=arreglo[cima-1];
+            arreglo[cima-1]=null;
+            cima--;
+        }
+        return temp;
+    }
 }
